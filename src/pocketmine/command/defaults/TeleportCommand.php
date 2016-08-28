b82dd78112c5385a8fdf56387c71119f6d224995
caspervanneck <nethercube.host@gmail.com>
2016-08-28 13:47:02 +0200

    fix tp

--- src/pocketmine/command/defaults/TeleportCommand.php
+++ src/pocketmine/command/defaults/TeleportCommand.php
@@ -1,24 +1,4 @@
 <?php
-
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
-*/
-
 namespace pocketmine\command\defaults;
 
 use pocketmine\command\Command;
@@ -44,6 +24,7 @@ class TeleportCommand extends VanillaCommand{
 			return true;
 		}
 
+		$args = array_filter($args);
 		if(count($args) < 1 or count($args) > 6){
 			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
 
@@ -88,12 +69,7 @@ class TeleportCommand extends VanillaCommand{
 		}
 
 		if(count($args) < 3){
-			if($origin->getLevel()->getDimension() === $target->getDimension()){
-				$origin->teleport($target);
-			} else {
-				$sender->sendMessage("Teleportation is impossible, because players are in different dimensions.");
-				return true;
-			}
+			$origin->teleport($target);
 			Command::broadcastCommandMessage($sender, new TranslationContainer("commands.tp.success", [$origin->getName(), $target->getName()]));
 
 			return true;
