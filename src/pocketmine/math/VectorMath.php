5c562c17c8f0be83413a36dcdb019a3a9233b34d
NycuRO <nghergu@yahoo.com>
2016-09-01 16:54:12 +0300

    Update VectorMath.php

--- src/pocketmine/math/VectorMath.php
+++ src/pocketmine/math/VectorMath.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -18,19 +17,12 @@
  * 
  *
 */
-
 namespace pocketmine\math;
-
-
 abstract class VectorMath{
-
 	public static function getDirection2D($azimuth) : Vector2{
 		return new Vector2(cos($azimuth), sin($azimuth));
 	}
-
 	public static function getDirection3D($azimuth, $inclination) : Vector3{
 		$yFact = cos($inclination);
 		return new Vector3($yFact * cos($azimuth), sin($inclination), $yFact * sin($azimuth));
 	}
-
-}
