33d69f072dacc9b0b744b523b2f65eafee0b2567
NycuRO <nghergu@yahoo.com>
2016-09-01 17:01:00 +0300

    Update Clay.php

--- src/pocketmine/block/Clay.php
+++ src/pocketmine/block/Clay.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -18,32 +17,22 @@
  * 
  *
 */
-
 namespace pocketmine\block;
-
 use pocketmine\item\Item;
 use pocketmine\item\Tool;
-
 class Clay extends Solid{
-
 	protected $id = self::CLAY_BLOCK;
-
 	public function __construct(){
-
 	}
-
 	public function getHardness() {
 		return 0.6;
 	}
-
 	public function getToolType(){
 		return Tool::TYPE_SHOVEL;
 	}
-
 	public function getName() : string{
 		return "Clay Block";
 	}
-
 	public function getDrops(Item $item) : array {
 		return [
 			[Item::CLAY, 0, 4],
