d1a00d66fd13872b03d01a10008d22b9b09d04ea
NycuRO <nghergu@yahoo.com>
2016-09-01 16:48:20 +0300

    Update Pond.php

--- src/pocketmine/level/generator/object/Pond.php
+++ src/pocketmine/level/generator/object/Pond.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -18,27 +17,21 @@
  * 
  *
 */
-
 namespace pocketmine\level\generator\object;
-
 use pocketmine\block\Block;
 use pocketmine\level\ChunkManager;
 use pocketmine\math\Vector3 as Vector3;
 use pocketmine\utils\Random;
-
-class Pond{
+use pocketmine\level\generator\object\Object;
+class Pond extends Object{
 	private $random;
 	public $type;
-
 	public function __construct(Random $random, Block $type){
 		$this->type = $type;
 		$this->random = $random;
 	}
-
 	public function canPlaceObject(ChunkManager $level, Vector3 $pos){
 	}
-
 	public function placeObject(ChunkManager $level, Vector3 $pos){
 	}
-
 }
