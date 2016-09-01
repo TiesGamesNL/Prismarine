445716d7fbe0a37a4fba5ae3980b5293a7d5be10
NycuRO <nghergu@yahoo.com>
2016-09-01 16:41:37 +0300

    Update Tree.php

--- src/pocketmine/level/generator/populator/Tree.php
+++ src/pocketmine/level/generator/populator/Tree.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -18,38 +17,23 @@
  * 
  *
 */
-
 namespace pocketmine\level\generator\populator;
-
 use pocketmine\block\Block;
 use pocketmine\block\Sapling;
 use pocketmine\level\ChunkManager;
 use pocketmine\level\generator\object\Tree as ObjectTree;
 use pocketmine\utils\Random;
-
-class Tree extends Populator{
+use pocketmine\level\generator\populator\VariableAmountPopulator;
+class Tree extends VariableAmountPopulator{
 	/** @var ChunkManager */
 	private $level;
-	private $randomAmount;
-	private $baseAmount;
-
 	private $type;
-
 	public function __construct($type = Sapling::OAK){
 		$this->type = $type;
 	}
-
-	public function setRandomAmount($amount){
-		$this->randomAmount = $amount;
-	}
-
-	public function setBaseAmount($amount){
-		$this->baseAmount = $amount;
-	}
-
 	public function populate(ChunkManager $level, $chunkX, $chunkZ, Random $random){
 		$this->level = $level;
-		$amount = $random->nextRange(0, $this->randomAmount + 1) + $this->baseAmount;
+		$amount = $this->getAmount($random);
 		for($i = 0; $i < $amount; ++$i){
 			$x = $random->nextRange($chunkX << 4, ($chunkX << 4) + 15);
 			$z = $random->nextRange($chunkZ << 4, ($chunkZ << 4) + 15);
@@ -60,7 +44,6 @@ class Tree extends Populator{
 			ObjectTree::growTree($this->level, $x, $y, $z, $random, $this->type);
 		}
 	}
-
 	private function getHighestWorkableBlock($x, $z){
 		for($y = 127; $y > 0; --$y){
 			$b = $this->level->getBlockIdAt($x, $y, $z);
@@ -70,7 +53,6 @@ class Tree extends Populator{
 				return -1;
 			}
 		}
-
 		return ++$y;
 	}
 }
